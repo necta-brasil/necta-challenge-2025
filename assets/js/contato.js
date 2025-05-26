7e3f66dfdd4a258f8b28b705bb285284c550c345
felipe rosseto <feliperossetorodrigues@gmail.com>
2025-05-25 23:53:06 -0300

    Anabelle_Faq

--- assets/js/contato.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('contato-form').addEventListener('submit', function(e) {
-    e.preventDefault();
-    alert('Mensagem enviada! Obrigado pelo contato.');
-    this.reset();
-});
--- assets/js/form.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('form-paciente').addEventListener('submit', function(e) {
-    e.preventDefault();
-    // Para a gnt validar e armazenar os dados :)
-    window.location.href = "confirmacao.html";
-});
